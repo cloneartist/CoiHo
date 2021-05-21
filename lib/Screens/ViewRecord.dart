fd0661f913a81e9186a49429c6b692b1aab714db
you@_simon <sonuisplaying@gmail.com>
2021-05-21 11:15:20 +0530

    create pdf and table structure in view records

--- lib/Screens/ViewRecord.dart
+++ lib/Screens/ViewRecord.dart
@@ -12,24 +12,24 @@ class ViewRecord extends StatefulWidget {
 class _ViewRecordState extends State<ViewRecord> {
   // Delete this
 
-  // List<Track> vehicles = [
-  //   new Track(
-  //     pressure: "s",
-  //     pulse: "s",
-  //     respirRate: "927393",
-  //     spO2: "w",
-  //     temp: "sd",
-  //     time: "sd",
-  //   ),
-  //   new Track(
-  //     pressure: "sert",
-  //     pulse: "sdt",
-  //     respirRate: "sd",
-  //     spO2: "sgjf",
-  //     temp: "sfg",
-  //     time: "sghj",
-  //   ),
-  // ];
+  List<Track> vehicles = [
+    new Track(
+      pressure: "s",
+      pulse: "s",
+      respirRate: "927393",
+      spO2: "w",
+      temp: "sd",
+      time: "sd",
+    ),
+    new Track(
+      pressure: "sert",
+      pulse: "sdt",
+      respirRate: "sd",
+      spO2: "sgjf",
+      temp: "sfg",
+      time: "sghj",
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -87,7 +87,8 @@ class _ViewRecordState extends State<ViewRecord> {
                           Text(selected.patient.name, style: TxtTheme),
                           Text(selected.patient.age, style: TxtTheme),
                           Text(selected.patient.opNo, style: TxtTheme),
-                          Text(selected.patient.phone??"Nil", style: TxtTheme),
+                          Text(selected.patient.phone ?? "Nil",
+                              style: TxtTheme),
                           Text(selected.patient.bedNo, style: TxtTheme),
                           Text(selected.patient.critical,
                               style: TxtTheme), // if possible else delete
@@ -108,128 +109,151 @@ class _ViewRecordState extends State<ViewRecord> {
                   decoration: BoxDecoration(
                       border: Border.all(color: Colors.blue, width: 2),
                       borderRadius: BorderRadius.circular(20)),
-                  height: MediaQuery.of(context).size.height * 0.6,
+                  height: MediaQuery.of(context).size.height * 0.7,
                   child: Padding(
                     padding: const EdgeInsets.all(10.0),
-                    child: ListView.builder(
-                      itemCount: selected.patient.track.length,
-                      itemBuilder: (context, i) {
-                        if (selected.patient.track[i].time == "null") {
-                          print("\n\n\n\n\nti,e null");
-                          return Container();
-                        }
-                        // if()
-                        var values = [];
-                        values.add(selected.patient.track[i].pulse == null
-                            ? "N/A"
-                            : selected.patient.track[i].pulse);
-                        values.add(selected.patient.track[i].temp == null
-                            ? "N/A"
-                            : selected.patient.track[i].temp);
-                        values.add(selected.patient.track[i].spO2 == null
-                            ? "N/A"
-                            : selected.patient.track[i].spO2);
-                        values.add(selected.patient.track[i].time == null
-                            ? "N/A"
-                            : selected.patient.track[i].time);
-                        values.add(selected.patient.track[i].respirRate == null
-                            ? "N/A"
-                            : selected.patient.track[i].respirRate);
-                        values.add(selected.patient.track[i].pressure == null
-                            ? "N/A"
-                            : selected.patient.track[i].pressure);
-                        print(values);
-                        return new ExpansionTile(
-                          leading: IconButton(
-                            icon: Icon(Icons.edit),
-                            onPressed: () {
-                              // selected.changeSelect()
-                              print(
-                                  "\n\nbefor nav ${selected.patient.track[i].spO2} ");
-                              // selected.changeSelect(pa)
-                              //TODO: Goto edit (Permission to edit for doc only)
-                              print("Edit button clicked!");
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => Nurse(
-                                        edit: "Yes",
-                                        index: i,
-                                        pat: selected.patient)),
-                              );
-                            },
-                          ),
-                          backgroundColor: Colors.white,
-                          subtitle: Text(
-                            "4 hours ago",
-                            style: Theme.of(context).textTheme.subtitle2,
-                          ),
-                          //Use builder
-                          title: new Text(
-                            "Record " + (i + 1).toString(),
-                            style: Theme.of(context)
-                                .textTheme
-                                .headline6
-                                .copyWith(color: Colors.blue),
-                          ),
-                          children: <Widget>[
-                            Card(
-                              shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(10),
-                                      bottomRight: Radius.circular(10))),
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceAround,
-                                  children: [
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          'SpO2: ' + values[2],
-                                          style: TxtTheme,
-                                        ),
-                                        Text(
-                                          'Pulse: ' + values[0],
-                                          style: TxtTheme,
-                                        ),
-                                        Text(
-                                          'Temp: ' + values[1],
-                                          style: TxtTheme,
-                                        ),
-                                      ],
-                                    ),
-                                    Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          'Pressure: ' + values[5],
-                                          style: TxtTheme,
-                                        ),
-                                        Text(
-                                          'Resp. rate: ' + values[4],
-                                          style: TxtTheme,
-                                          softWrap: true,
-                                        ),
-                                        Text(
-                                          'Time: ' + values[3],
-                                          style: TxtTheme,
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            )
-                          ],
-                        );
-                      },
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.horizontal,
+                      child: DataTable(
+                        columns: [
+                          DataColumn(label: Text('TIMESTAMP')),
+                          DataColumn(label: Text('TEMPERATURE')),
+                          DataColumn(label: Text('PULSE')),
+                          DataColumn(label: Text('RESPIRATory\nRATE')),
+                          DataColumn(label: Text('SPO2')),
+                          DataColumn(label: Text('PRESSURE')),
+                        ],
+                        rows: selected.patient.track
+                            .map((track) => DataRow(cells: [
+                                  DataCell(Text(track.time ?? 'N/A')),
+                                  DataCell(Text(track.temp ?? 'N/A')),
+                                  DataCell(Text(track.pulse ?? 'N/A')),
+                                  DataCell(Text(track.respirRate ?? 'N/A')),
+                                  DataCell(Text(track.spO2 ?? 'N/A')),
+                                  DataCell(Text(track.pressure ?? 'N/A')),
+                                ]))
+                            .toList(),
+                      ),
                     ),
+                    // child: ListView.builder(
+                    //   itemCount: selected.patient.track.length,
+                    //   itemBuilder: (context, i) {
+                    //     if (selected.patient.track[i].time == "null") {
+                    //       print("\n\n\n\n\nti,e null");
+                    //       return Container();
+                    //     }
+
+                    //     var values = [];
+                    //     values.add(selected.patient.track[i].pulse == null
+                    //         ? "N/A"
+                    //         : selected.patient.track[i].pulse);
+                    //     values.add(selected.patient.track[i].temp == null
+                    //         ? "N/A"
+                    //         : selected.patient.track[i].temp);
+                    //     values.add(selected.patient.track[i].spO2 == null
+                    //         ? "N/A"
+                    //         : selected.patient.track[i].spO2);
+                    //     values.add(selected.patient.track[i].time == null
+                    //         ? "N/A"
+                    //         : selected.patient.track[i].time);
+                    //     values.add(selected.patient.track[i].respirRate == null
+                    //         ? "N/A"
+                    //         : selected.patient.track[i].respirRate);
+                    //     values.add(selected.patient.track[i].pressure == null
+                    //         ? "N/A"
+                    //         : selected.patient.track[i].pressure);
+                    //     print(values);
+                    //     return new ExpansionTile(
+                    //       leading: IconButton(
+                    //         icon: Icon(Icons.edit),
+                    //         onPressed: () {
+                    //           // selected.changeSelect()
+                    //           print(
+                    //               "\n\nbefor nav ${selected.patient.track[i].spO2} ");
+                    //           // selected.changeSelect(pa)
+                    //           //TODO: Goto edit (Permission to edit for doc only)
+                    //           print("Edit button clicked!");
+                    //           Navigator.push(
+                    //             context,
+                    //             MaterialPageRoute(
+                    //                 builder: (context) => Nurse(
+                    //                     edit: "Yes",
+                    //                     index: i,
+                    //                     pat: selected.patient)),
+                    //           );
+                    //         },
+                    //       ),
+                    //       backgroundColor: Colors.white,
+                    //       subtitle: Text(
+                    //         "4 hours ago",
+                    //         style: Theme.of(context).textTheme.subtitle2,
+                    //       ),
+                    //       //Use builder
+                    //       title: new Text(
+                    //         "Record " + (i + 1).toString(),
+                    //         style: Theme.of(context)
+                    //             .textTheme
+                    //             .headline6
+                    //             .copyWith(color: Colors.blue),
+                    //       ),
+                    //       children: <Widget>[
+                    //         Card(
+                    //           shape: RoundedRectangleBorder(
+                    //               borderRadius: BorderRadius.only(
+                    //                   bottomLeft: Radius.circular(10),
+                    //                   bottomRight: Radius.circular(10))),
+                    //           child: Padding(
+                    //             padding:
+                    //                 const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                    //             child: Row(
+                    //               mainAxisAlignment:
+                    //                   MainAxisAlignment.spaceAround,
+                    //               children: [
+                    //                 Column(
+                    //                   crossAxisAlignment:
+                    //                       CrossAxisAlignment.start,
+                    //                   children: [
+                    //                     Text(
+                    //                       'SpO2: ' + values[2],
+                    //                       style: TxtTheme,
+                    //                     ),
+                    //                     Text(
+                    //                       'Pulse: ' + values[0],
+                    //                       style: TxtTheme,
+                    //                     ),
+                    //                     Text(
+                    //                       'Temp: ' + values[1],
+                    //                       style: TxtTheme,
+                    //                     ),
+                    //                   ],
+                    //                 ),
+                    //                 Column(
+                    //                   crossAxisAlignment:
+                    //                       CrossAxisAlignment.start,
+                    //                   children: [
+                    //                     Text(
+                    //                       'Pressure: ' + values[5],
+                    //                       style: TxtTheme,
+                    //                     ),
+                    //                     Text(
+                    //                       'Resp. rate: ' + values[4],
+                    //                       style: TxtTheme,
+                    //                       softWrap: true,
+                    //                     ),
+                    //                     Text(
+                    //                       'Time: ' + values[3],
+                    //                       style: TxtTheme,
+                    //                     ),
+                    //                   ],
+                    //                 ),
+                    //               ],
+                    //             ),
+                    //           ),
+                    //         )
+                    //       ],
+                    //     );
+                    //   },
+                    // ),
                   ),
                 ),
               ],
